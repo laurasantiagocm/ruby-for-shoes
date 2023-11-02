45f5a6a8abedeed1c49443e30511809bb4971136
Laura Santiago <laura.santiago.co@gmail.com>
2023-11-02 13:13:25 -0300

    First commit: procedural and buggy application

---
+++ main.rb
@@ -0,0 +1,197 @@
+$gm_name = 'Mestre'
+
+$player_name = 'Fulano'
+$player_xp = 0
+$player_statuses = [
+  {
+    name: 'poison',
+    influence: -2,
+    skills: ['Do anything']
+  },
+  {
+    name: 'leg day',
+    influence: 2,
+    skills: ['Run', 'Walk']
+  }
+]
+
+$player_skills = [
+  {
+    name: "Do anything",
+    level: 1,
+    subskills: [
+      {
+        name: "Walk",
+        level: 2,
+        subskills: [
+          {
+            name: "Walk at home",
+            level: 3
+          }
+        ]
+      },
+      {
+        name: "Run",
+        level: 2,
+        subskills: []
+      }
+    ]
+  }
+]
+
+def player_display_not_allowed
+  p "Display not allowed for different players"
+end
+
+def print_skills(interested_name, structure, indent = 0)
+  return player_display_not_allowed if interested_name != $player_name && interested_name != $gm_name
+  structure.each do |item|
+    item.each do |key, value|
+      if key == :subskills && value.is_a?(Array) && !value.empty?
+        puts "  " * indent + "#{key}:"
+        print_skills(interested_name, value, indent + 1)
+      else
+        puts "  " * indent + "#{key}: #{value}"
+      end
+    end
+  end
+end
+
+print_skills('Fulano', $player_skills)
+
+def extract_skill_names(structure)
+  skill_names = []
+  return skill_names if structure.nil?
+
+  structure.each do |item|
+    item.each do |key, value|
+      if key == :name
+        skill_names << value
+      elsif key == :subskills && value.is_a?(Array) && !value.empty?
+        value.each do |subskill|
+          skill_names << subskill[:name]
+          skill_names.concat(extract_skill_names(subskill[:subskills]))
+        end
+      end
+    end
+  end
+  skill_names
+end
+
+def player_display_status
+  $player_statuses.each do |status|
+    next if (status[:skills] && extract_skill_names($player_skills)).empty?
+    p "Player is affected by: #{status[:name]} (#{status[:influence]})"
+  end
+end
+
+def player_roll_for_skill(skill)
+  puts "Checking for statuses"
+  affecting_statuses = $player_statuses.select { |status| status[:skills].include?(skill[:name]) }
+  status_affection_dice = affecting_statuses.sum{ |s| s[:influence] } if affecting_statuses.any?
+
+  player_display_status
+
+  p "Rolling for #{skill[:name]} - #{skill[:level]}d6 (status influence: #{status_affection_dice})"
+  die_rolls = []
+  skill[:level].times do
+    die_rolls << rand(1..6)
+  end
+
+  if !status_affection_dice.nil?
+    if status_affection_dice > 0
+      status_affection_dice.times do
+        die_rolls << rand(1..6)
+      end
+    elsif status_affection_dice < 0
+      status_affection_dice.abs.times do
+        die_rolls << (rand(1..6) * -1)
+      end
+    end
+  end
+
+  result = die_rolls.sum
+  p "#{result} (#{die_rolls.join(' + ')})"
+  result
+end
+
+player_roll_for_skill($player_skills[0][:subskills][0])
+player_roll_for_skill($player_skills[0])
+
+def dm_roll(difficulty)
+  p "Rolling #{difficulty}d6 for DM. Good luck >:)"
+  die_rolls = []
+  difficulty.times do
+    die_rolls << rand(1..6)
+  end
+
+  result = die_rolls.sum
+  p "#{result} (#{die_rolls.join(' + ')})"
+  result
+end
+
+def player_display_not_found_skill(name)
+  p "Skill #{name} was not found"
+end
+
+dm_roll(3)
+
+def increase_level(structure, target_subskill_name, new_subskill)
+  subskill_found = false
+  structure.each do |item|
+    item.each do |key, value|
+      if key == :subskills && value.is_a?(Array) && !value.empty?
+        updated_subskills = []
+        subskill_found = false
+
+        value.each do |subskill|
+          if subskill[:name] == target_subskill_name
+            subskill_found = true
+            if !subskill[:subskills]
+              subskill[:subskills] = []
+            end
+            subskill[:subskills] << new_subskill
+            updated_subskills << subskill
+          else
+            updated_subskills << subskill
+          end
+        end
+
+        item[:subskills] = updated_subskills if subskill_found
+        increase_level(updated_subskills, target_subskill_name, new_subskill)
+      end
+    end
+  end
+
+  if !subskill_found
+    return player_display_not_found_skill(target_subskill_name)
+  end
+
+  print_skills('Fulano', $player_skills)
+end
+
+new_subskill = { name: "Walk at home while sleepy", level: 4 }
+increase_level($player_skills, "Walk at home", new_subskill)
+
+new_status = {
+  name: 'bleeding',
+  influence: -3,
+  skills: ['Do anything']
+}
+
+def add_status(status)
+  $player_statuses << status
+  puts "Status #{status[:name]} was added."
+  player_display_status
+end
+
+def remove_status(status_name)
+  $player_statuses = $player_statuses.select { |s| s[:name] != status_name }
+  puts "Status #{status_name} was removed."
+  player_display_status
+end
+
+add_status(new_status)
+remove_status('poison')
+
+player_roll_for_skill($player_skills[0][:subskills][0])
